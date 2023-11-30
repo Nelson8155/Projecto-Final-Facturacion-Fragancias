27c3a32edd28af9b4a8376a47ad99898790bf541
unknown <nelsonbeltran491@gmail.com>
2023-11-29 23:04:56 -0600

    actualizacion 2

--- src/main/java/com/fragansias/company/controller/dto/ItemFacturaControllerDTO.java
+++ src/main/java/com/fragansias/company/controller/dto/ItemFacturaControllerDTO.java
@@ -20,6 +20,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/itemFactura")
@@ -83,6 +84,8 @@ public class ItemFacturaControllerDTO extends GenericoControllerDTO<ItemFactura,
         response.put("data", dto);
         return ResponseEntity.ok(response);
     }
+
+
 
 
 
